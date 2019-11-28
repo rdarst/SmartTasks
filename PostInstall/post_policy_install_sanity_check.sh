79e49b2ed2e631c7c2b4dbcb81450697119de2f6
Jim Öqvist <jim@oqvist.org>
2019-11-28 17:18:58 +0100

    added funtion to report incident to ServiceNow

--- PostInstall/post_policy_install_sanity_check.sh
+++ PostInstall/post_policy_install_sanity_check.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+function servicenow_report_incident()
+{
+  curl_cli "https://dev71699.service-now.com/api/now/table/incident" \
+  --insecure \
+  --request POST \
+  --header "Accept:application/json" \
+  --header "Content-Type:application/json" \
+  --data "{\"short_description\":\"short descr Test incident creation through Check Point mgmt\",\"comments\":\"these are my rest comments caller sys admin\",\"active\":\"true\",\"caller_id\":\"System Administrator\",\"urgency\":\"1\",\"impact\":\"1\",\"priority\":\"1\"}" \
+  --user 'admin':'mypassword'
+} 
+
+
+
 
 trigger_json=$(echo $1 | base64 --decode -i)
 echo $trigger_json > /tmp/policy_install_trigger.json
@@ -16,7 +29,9 @@ sanity_check=$(curl_cli --silent --insecure --location --request GET "https://po
 # If the install policy succeeds, execute sanity checks
 if [[ $(echo $sanity_check) != "true" ]]; then
   mgmt_cli -r true install-policy policy-package "standard" access true targets.1 "smsg60" revision "18d4f80f-fd8e-4ac4-b690-5597b0cb5397" --format json > /tmp/revert.json
-  sanity_notification=$(echo $trigger_json | jq '("Policy: " + .tasksResult[].target + ", installed by: " + .initiator + ", failed sainty check of critical services, reverting to last good known policy, ticket has been open for: " + .initiator + " to validate the policy")')
+  sanity_notification=$(echo $trigger_json | jq  '{"Policy": .tasksResult[].target,"installed by": .initiator,"message": "failed sainty check of critical services, reverting to last good known policy, request to validate the policy have been sent to the intiator of the policy install"}'
   echo $sanity_notification > /tmp/policy_sanity_notification.out
   exit 1
 fi
+
+
