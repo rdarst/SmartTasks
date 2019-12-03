a9d6f590d158acafb75b0ab347e048480421c21f
rdarst <45427115+rdarst@users.noreply.github.com>
2019-12-03 15:24:57 -0500

    Updated for Cluster operations

--- PostInstall/policy_sanity_check_script.sh
+++ PostInstall/policy_sanity_check_script.sh
@@ -2,44 +2,65 @@
 
 # base64 decoding and storing the SmartTask JSON response
 trigger_json=$(echo $1 | base64 --decode -i)
-echo $trigger_json > /tmp/policy_install_trigger.json
+#echo $trigger_json > /tmp/policy_install_trigger.json
 
 # Getting initiator of policy install
 install_policy_initiator=$(echo $trigger_json | jq -r .initiator)
 # Getting the end result of the policy install
 install_policy_result=$(echo $trigger_json | jq .installPolicyResult)
 # Getting the policy package name from the policy install
-install_policy_package=$(echo $trigger_json | jq -r .tasksResult[].target)
+install_policy_package=$(echo $trigger_json | jq -r .policyPackage)
 # Getting the targets for the policy install
-install_policy_targets=$(echo $trigger_json | jq -r .policyPackage)
-# Getting the ServiceNow user to be used by SmartTask to creat a incident in ServiceNow
+install_policy_targets=$(echo $trigger_json | jq -r .tasksResult[].target)
+# Getting the ServiceNow user to be used by SmartTask to create an incident in ServiceNow
 smarttask_servicenow_user=$(echo $trigger_json | jq -r '."custom-data".smarttask_servicenow_user')
-# Getting the ServiceNow password to be used by SmartTask to creat a incident in ServiceNow
+# Getting the ServiceNow password to be used by SmartTask to create an incident in ServiceNow
 smarttask_servicenow_password=$(echo $trigger_json | jq -r '."custom-data".smarttask_servicenow_password')
-# Getting the short description to be used in the incident created in ServiceNow 
+# Getting the ServiceNow Host to be used by SmartTask to create an incident in ServiceNow
+smarttask_servicenow_host=$(echo $trigger_json | jq -r '."custom-data".smarttask_servicenow_host')
+# Getting the short description to be used in the incident created in ServiceNow
 short_description=$(echo $trigger_json | jq -r '."custom-data".short_description')
-# Getting the short description to be used in the incident created in ServiceNow 
+# Getting the short description to be used in the incident created in ServiceNow
 comments=$(echo $trigger_json | jq -r '."custom-data".comments')
+
+# Get the number of targets and build a string from the json
+num_of_targets=`echo "$install_policy_targets" |wc -l`
+i=1
+targets=""
+install_targets=""
+while read -r line
+do
+   targets+="$line"
+   install_targets+="targets.$i $line "
+   if [ $i -lt $num_of_targets ]
+   then
+     targets+=","
+     i=$((i+1))
+   fi
+done  <<<"$install_policy_targets"
+
 
 # This function will revert the policy to a previous revision
 function revert_to_last_good_known_policy()
 {
   mgmt_cli -r true --format json install-policy \
-  policy-package "$install_policy_package" access true targets.1 "$install_policy_targets" \
-  revision "18d4f80f-fd8e-4ac4-b690-5597b0cb5397" > /tmp/revert.json
+  policy-package "$install_policy_package" access true \
+  revision "d43eff97-0c56-48f6-9bb4-8ae002f166d3" > /tmp/revert.json
 }
 
-# This funtion will create a servicenow incident ticket using the servicenow API
+# This function will create a servicenow incident ticket using the servicenow API
 function servicenow_report_incident()
 {
-  curl_cli "https://dev71699.service-now.com/api/now/table/incident" \
+    json_post_data="{\"short_description\":\"${short_description} ${targets}\", \"comments\":\"${install_policy_package} ${comments}\", \"active\": \"true\", \"caller_id\": \"$install_policy_initiator\", \"urgency\": \"1\", \"impact\": \"1\", \"priority\": \"1\"}"
+  echo "$json_post_data" > /tmp/api_out.txt
+  curl_cli "https://${smarttask_servicenow_host}/api/now/table/incident" \
   --insecure \
   --request POST \
   --header "Accept:application/json" \
   --header "Content-Type:application/json" \
-  --data '{"short_description":"'"$short_description $install_policy_targets"'", "comments":"'"$install_policy_package $comments"'", "active": "true", "caller_id": "'"$install_policy_initiator"'", "urgency": "1", "impact": "1", "priority": "1"}' \
+  --data "$json_post_data" \
   --user "$smarttask_servicenow_user":"$smarttask_servicenow_password"
-} 
+}
 
 # This function will verify that the CPX360 business critical traffic is working after policy install
 function sanity_check()
@@ -49,7 +70,7 @@ function sanity_check()
   --insecure \
   --location \
   --request GET  \
-  | jq '.Event | contains("CPX360_is_kicking")'
+  | jq '.Event | contains("CPX360_is_kicking!")'
 }
 
 # If the install policy fails, exit without error
@@ -63,6 +84,7 @@ sanity_check_result=$(sanity_check)
 # If the install policy succeeds, execute sanity checks
 if [[ $(echo $sanity_check_result) != "true" ]]; then
   servicenow_report_incident &
+  sleep 5
   revert_to_last_good_known_policy
   exit 1
 fi
