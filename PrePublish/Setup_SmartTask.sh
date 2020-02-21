df8712f6694c48055f64ec7e978680de6efb6da6
rdarst <45427115+rdarst@users.noreply.github.com>
2020-02-21 08:36:02 -0500

    Updated repo location

--- PrePublish/Setup_SmartTask.sh
+++ PrePublish/Setup_SmartTask.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 varSid=$(mgmt_cli -r true -f json login | jq -r '.sid')
-curl_cli -kLs https://raw.githubusercontent.com/jimoq/SmartTasks/master/PrePublish/Prepublish.sh > pre-publish-tag-check.sh
+curl_cli -kLs https://raw.githubusercontent.com/rdarst/SmartTasks/master/PrePublish/Prepublish.sh > pre-publish-tag-check.sh
 read -p "Enter the admin you want to check (e.g.: api_user): " allowed_admin
 read -p "Enter the tag name you tagged your objects with (e.g.: Cloud): " allowed_tag
 mgmt_cli --session-id $varSid -f json add-tag name "$allowed_tag" color "violet red" | jq .name
